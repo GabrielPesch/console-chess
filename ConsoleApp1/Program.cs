d5974f884cf7107af3926831892e825f91fe1877
Gabriel Pesch <gkpesch@gmail.com>
2023-01-28 20:11:35 -0300

    feat: Criando classe Position

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using board;
+using System;
+
+namespace console_chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position;
+            position = new Position(3, 4);
+
+            Console.WriteLine("Test Position: " + position);
+
+            Console.ReadLine();
+        }
+    }
+}
