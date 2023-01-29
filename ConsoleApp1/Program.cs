275a2ea76232b5e9b755c3665ff252c5e3ee2970
Gabriel Pesch <gkpesch@gmail.com>
2023-01-28 21:42:27 -0300

    feat: Melhorando visibilidade das peças

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,26 @@ namespace console_chess
     {
         static void Main(string[] args)
         {
-            ChessPosition pos = new ChessPosition('c', 7);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(pos.toPosition());
+                board.setPiecePosition(new Rook(board, Color.Black), new Position(0, 0));
+                board.setPiecePosition(new Rook(board, Color.Black), new Position(1, 3));
+                board.setPiecePosition(new King(board, Color.Black), new Position(2, 4));
 
+
+                board.setPiecePosition(new Rook(board, Color.White), new Position(3,2));
+                board.setPiecePosition(new Rook(board, Color.White), new Position(4, 6));
+                board.setPiecePosition(new King(board, Color.White), new Position(5, 7));
+                Screen.printBoard(board);
+
+                Console.ReadLine();
+            }
+            catch (BoardException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
--- ConsoleApp1/Screen.cs
+++ ConsoleApp1/Screen.cs
@@ -9,6 +9,7 @@ namespace chess
         {
             for (int currentLine = 0; currentLine < board.lines; currentLine++)
             {
+                Console.Write(8 - currentLine + " ");
                 for (int currentColumn = 0; currentColumn < board.columns; currentColumn++)
                 {
                     if (board.piece(currentLine, currentColumn) == null)
@@ -17,11 +18,28 @@ namespace chess
                     }
                     else
                     {
-                        Console.Write(board.piece(currentLine, currentColumn) + " ");
+                        printPiece(board.piece(currentLine, currentColumn));
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void printPiece(Piece piece)
+        {
+            if (piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+            Console.Write(" ");
         }
     }
 }
